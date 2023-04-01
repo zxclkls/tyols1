e9510a15d18c4451d00f352266cce650a9c422e5
Аліна Романенко <129578351+zxclkls@users.noreply.github.com>
2023-04-01 08:35:23 +0000

    money

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+import time
 class Person:
   age = 14
   height = 157.3
+  money = 0
   isMale = True
   neme = 'nikita'
 
 
-me = Person()
+  def __init__(self, neme, age):
+    self.neme = neme
+    self.age = age
+  
+  def work(self, days):
+  
+   while days != 0:
+    time.sleep(1)
+    self.money += 1
+    days -= 1
 
-print(me.age)
-print(me.height)
-print(me.isMale)
-print(me.neme)
 
-friend = Person()
-friend.neme = "Volody"
+
+me = Person('Volody', 15)
+friend = Person('Slavik', 16)
+
+print(me.money)
+me.work(31)
+print(me.money)
+
